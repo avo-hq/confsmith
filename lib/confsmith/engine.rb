b28d0bb3799da1ef1f08e50d764758a5cb564725
Adrian <adrian@adrianthedev.com>
2025-07-02 15:41:42 +0300

    add importmap support

--- lib/confsmith/engine.rb
+++ lib/confsmith/engine.rb
@@ -1,5 +1,19 @@
 module Confsmith
   class Engine < ::Rails::Engine
     isolate_namespace Confsmith
+
+    # Add our own importmap config to the main app
+    initializer "confsmith.importmap", before: "importmap" do |app|
+      if Rails.application.respond_to?(:importmap)
+        app.config.importmap.paths << Engine.root.join("config/importmap.rb")
+      end
+    end
+
+    # Add our the javascript directory to the assets path
+    initializer "confsmith.assets.precompile" do |app|
+      if Rails.application.config.respond_to?(:assets)
+        app.config.assets.paths << Engine.root.join("app/javascript/").to_s
+      end
+    end
   end
 end
